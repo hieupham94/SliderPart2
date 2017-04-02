88ce48f377f4841114080b0f9a0bfc3c9c77054b
hieu pham <hieu pham@DESKTOP-1BMEJC9>
2017-04-02 14:20:12 -0700

    Add project files.

---
+++ SliderPart2/SliderPart2/Puzzle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SliderPart2
+{
+    public class Puzzle : ContentPage
+    {
+        public Puzzle()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello Page" }
+                }
+            };
+        }
+    }
+}
